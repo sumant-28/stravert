7aaa8aad6f6c9e32aef9db0c2b4b4d1699920d87
Sumanth Athreya Seshasayee <sumant28@hotmail.com>
2025-11-04 07:43:12 +1300

    Update Home.py
    Claude

--- Home.py
+++ Home.py
@@ -12,15 +12,14 @@ st.set_page_config(page_icon="🏠", layout="wide")
 # Display
 st.title("🏠 Home")
 
-d1, d2, d3 = utils.get_dfs()
-
-d1 = utils.fix_all_timestamps_in_df(d1)
-d2 = utils.fix_all_timestamps_in_df(d2)
-d3 = utils.fix_all_timestamps_in_df(d3)
-
-if d1 not in st.session_state:
+# Check if dataframes are already in session state
+if 'd1' not in st.session_state or 'd2' not in st.session_state or 'd3' not in st.session_state:
+    d1, d2, d3 = utils.get_dfs()
+    
+    d1 = utils.fix_all_timestamps_in_df(d1)
+    d2 = utils.fix_all_timestamps_in_df(d2)
+    d3 = utils.fix_all_timestamps_in_df(d3)
+    
     st.session_state.d1 = d1
-if d2 not in st.session_state:
     st.session_state.d2 = d2
-if d3 not in st.session_state:
     st.session_state.d3 = d3
